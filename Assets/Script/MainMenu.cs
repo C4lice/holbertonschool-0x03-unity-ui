c176d542a747c8ee52f35277e8efc100f2413363
C4lice <laura.aupetit002@gmail.com>
2026-02-02 00:16:43 +0100

    Adds colour blindness mode support to the main menu and improves the structure of user interface objects.

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     public void PlayMaze()
     {
+        // Vérifie si le mode daltonien est activé
+        if (colorblindMode.isOn)
+        {
+            // Orange pour les pièges
+            trapMat.color = new Color32(255, 112, 0, 1);
+
+            // Bleu pour l'objectif
+            goalMat.color = Color.blue;
+        }
+
+        // Charge la scène du labyrinthe
         SceneManager.LoadScene("maze");
     }
+
     public void QuitMaze()
-{
-    Debug.Log("Quit Game");
-    Application.Quit();
-}
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
 }
