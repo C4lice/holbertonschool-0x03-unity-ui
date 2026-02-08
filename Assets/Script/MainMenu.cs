30bf01edb9b737d1916cd77b0730ce35a08d9f23
C4lice <laura.aupetit002@gmail.com>
2026-02-08 17:16:05 +0100

    Refactor code structure for improved readability and maintainability

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,32 +1,66 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button PlayButton;
+
+    public Button QuitButton;
+
+    public Button OptionButton;
+
+    public Button BackButton;
+
+    public GameObject MainMenuPage;
+
+    public GameObject OptionPage;
+
     public Material trapMat;
+
     public Material goalMat;
+
     public Toggle colorblindMode;
 
-    public void PlayMaze()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        // Vérifie si le mode daltonien est activé
-        if (colorblindMode.isOn)
-        {
-            // Orange pour les pièges
-            trapMat.color = new Color32(255, 112, 0, 1);
-
-            // Bleu pour l'objectif
-            goalMat.color = Color.blue;
-        }
-
-        // Charge la scène du labyrinthe
-        SceneManager.LoadScene("maze");
+        PlayButton.onClick.AddListener(PlayButtonClicked);
+        OptionButton.onClick.AddListener(OptionButtonClicked);
+        QuitButton.onClick.AddListener(QuitButtonClicked);
+        BackButton.onClick.AddListener(BackButtonClicked);
     }
 
-    public void QuitMaze()
+    // Update is called once per frame
+    void PlayButtonClicked()
+    {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
+        SceneManager.LoadScene(sceneName:"maze"); 
+    }
+
+    void QuitButtonClicked()
     {
         Debug.Log("Quit Game");
-        Application.Quit();
+    }
+
+    void OptionButtonClicked()
+    {
+        MainMenuPage.gameObject.SetActive(false);
+        OptionPage.gameObject.SetActive(true);
+    }
+
+    void BackButtonClicked()
+    {
+        MainMenuPage.gameObject.SetActive(true);
+        OptionPage.gameObject.SetActive(false);
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,109 +3,105 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 
+// Contrôleur de joueur pour gérer les mouvements, les collisions et l'état du jeu.
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed = 5f;
-
-    public int health = 5;
+    private int score = 0;
 
     public Text scoreText;
 
     public Text healthText;
 
-    public GameObject winLoseBG;
-    public Text winLoseText;
+    public Text WinOrLoose;
+
+    public Image WinLoseBG;
+
+    public int health = 5;
+
+    [Tooltip("speed of the player")]
+    [SerializeField]
+    public float speed;
 
     private Rigidbody rb;
-
-    private int score = 0;
-
-    private bool gameEnded = false;
-    // Initialisation
-    void Start()
+    // Start est appelé avant la première mise à jour d'image.
+    public void Start()
     {
-
         rb = GetComponent<Rigidbody>();
-        SetScoreText();
-        SetHealthText();
     }
-    // Gérer le mouvement du joueur
-    void FixedUpdate()
+    // La mise à jour est appelée une fois par image.
+    public void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-
-
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
-        // Position et déplacement du joueur
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-
-    }
-
-    void Update()
-    {
-        // Si le joueur appuie sur Echap
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // Si la santé du joueur atteint 0, affiche un message de défaite et recharge la scène après 3 secondes.
+        if (health == 0)
         {
-            SceneManager.LoadScene("menu");
-        }
-        // Vérifier la santé du joueur
-        if (health <= 0 && !gameEnded)
-        {
-            gameEnded = true;
-
-            winLoseBG.SetActive(true);
-            winLoseText.text = "Game Over!";
-            winLoseText.color = Color.white;
-            winLoseBG.GetComponent<Image>().color = Color.red;
-
+            WinLoseBG.color = Color.red;
+            WinOrLoose.color = Color.white;
+            WinOrLoose.text = "Game Over!";
+            WinLoseBG.gameObject.SetActive(true);
+            //Debug.Log("Game Over!");
             StartCoroutine(LoadScene(3));
         }
+        // Charger la scène du menu principal lorsque la touche Échap est enfoncée.
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            OnButtonClicked();
+        }
     }
-    // Gérer les collisions avec les pickups, les traps et les goals
+    // FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    public void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed;
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        // Pickup
-        if (other.CompareTag("Pickup"))
+        // Si le joueur entre en collision avec un objet de collecte, augmente son score de 1, met à jour le texte du score et détruit l'objet de collecte.
+        if (other.CompareTag("Pickup")) 
         {
-            score++;
+            score += 1;
+            //Debug.Log($"Score: {score}");
             SetScoreText();
-            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
         }
-        // Trap
-        if (other.CompareTag("Trap"))
+        // Si le joueur entre en collision avec un piège, réduit sa santé de 1 et met à jour le texte de la santé.
+        else if (other.CompareTag("Trap"))
         {
-            health--;
+            health -= 1;
             SetHealthText();
+            //Debug.Log($"Health: {health}");
         }
-        // Goal
-        if (other.CompareTag("Goal") && !gameEnded)
+        // Si le joueur atteint l'objectif, affiche un message de victoire et recharge la scène après 3 secondes.
+        else if (other.CompareTag("Goal"))
         {
-            gameEnded = true;
-
-            winLoseBG.SetActive(true);
-            winLoseText.text = "You Win!";
-            winLoseText.color = Color.black;
-            winLoseBG.GetComponent<Image>().color = Color.green;
-
+            WinLoseBG.color = Color.green;
+            WinOrLoose.color = Color.black;
+            WinOrLoose.text = "You Win!";
+            WinLoseBG.gameObject.SetActive(true);
             StartCoroutine(LoadScene(3));
+            //Debug.Log("You win!");
         }
     }
-    // Recharger la scène après un délai
+    // Met à jour le texte du score du joueur.
+    void SetScoreText()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+    // Met à jour le texte de la santé du joueur.
+    void SetHealthText()
+    {
+        healthText.text = $"Health: {health}";
+    }
+    // Coroutine pour charger la scène après un délai de 3 secondes.
     IEnumerator LoadScene(float seconds)
     {
         yield return new WaitForSeconds(seconds);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    // Mettre à jour le texte du score et de la santé
-    void SetScoreText()
+    // Charger la scène du menu principal lorsque la touche Échap est enfoncée.
+    void OnButtonClicked()
     {
-        scoreText.text = "Score: " + score;
-    }
-
-    void SetHealthText()
-    {
-        healthText.text = "Health: " + health;
+        SceneManager.LoadScene (sceneName:"menu");
     }
 }
